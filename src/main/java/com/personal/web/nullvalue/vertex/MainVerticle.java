a0a45f3eec480f295ef99ce8aa05d8dedb21aacc
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2020-06-15 23:43:07 -0300

    Introduce patch method to handle null value

--- src/main/java/com/personal/web/nullvalue/vertex/MainVerticle.java
+++ src/main/java/com/personal/web/nullvalue/vertex/MainVerticle.java
@@ -1,9 +1,20 @@
 package com.personal.web.nullvalue.vertex;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jsonpatch.JsonPatchException;
+import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 
 public class MainVerticle extends AbstractVerticle {
+
+  private Map<String, String> values = new HashMap<>();
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
@@ -19,5 +30,17 @@ public class MainVerticle extends AbstractVerticle {
         startPromise.fail(http.cause());
       }
     });
+  }
+
+  private void merge(final String json) throws IOException {
+    try {
+      JsonNode target = JsonLoader.fromString(json);
+      JsonNode source = JsonLoader.fromString(values.get("key"));
+
+      JsonMergePatch patch = JsonMergePatch.fromJson(source);
+      patch.apply(target);
+    } catch (JsonPatchException e) {
+      e.printStackTrace();
+    }
   }
 }
